80cae6fbfd53606671a2959db52c95e584d7fcb9
Sravya Putumbaka <sravyaputumbaka4@gmail.com>
2026-02-26 12:02:56 +0530

    fail case

--- test_app.py
+++ test_app.py
@@ -1,7 +1,7 @@
 import app
 
 def test_factorial_positive():
-    assert app.calculate_factorial(5) == 120
+    assert app.calculate_factorial(5) == 122
     assert app.calculate_factorial(3) == 6
 
 def test_factorial_zero():
