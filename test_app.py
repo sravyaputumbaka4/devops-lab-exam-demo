4338984e50b67cd51a0a0bea8f222d897bdfa302
Sravya Putumbaka <sravyaputumbaka4@gmail.com>
2026-02-26 12:05:30 +0530

    fail1

--- test_app.py
+++ test_app.py
@@ -6,11 +6,3 @@ def test_factorial_positive():
 
 def test_factorial_zero():
     assert app.calculate_factorial(0) == 1
-
-def test_factorial_negative():
-    try:
-        app.calculate_factorial(-1)
-    except ValueError:
-        assert True 
-    else:
-        assert False 
