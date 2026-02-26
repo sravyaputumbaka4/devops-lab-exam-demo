764fbdfd848caa48fc8d8c4ed8650b42e1d11847
Sravya Putumbaka <sravyaputumbaka4@gmail.com>
2026-02-26 12:09:45 +0530

    test

--- test_app.py
+++ test_app.py
@@ -1,5 +1,16 @@
-import app
+from app import calculate_factorial
 
 def test_factorial_positive():
     assert app.calculate_factorial(5) == 122
-    assert app.calculate_factorial(3) == 0
+    assert app.calculate_factorial(3) == 6
+
+def test_factorial_zero():
+    assert app.calculate_factorial(0) == 1
+
+def test_factorial_negative():
+    try:
+        app.calculate_factorial(-1)
+    except ValueError:
+        assert True 
+    else:
+        assert False 
